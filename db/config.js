8a761fb59f511fbb0a9c4b99a23d35048de5491d
Jean-Robert Rene Jr <Jean-Robert@J-Rs-MacBook-Pro.local>
2017-10-30 11:54:18 -0400

    implemented user_id to pick up search terms

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')({}),
-config = process.env.DATABASE_URL || 'postgres://rebekahsunmibae@localhost:5432/woke_db',
+config = process.env.DATABASE_URL || 'postgres://Jean-Robert@localhost:5432/woke_db',
 db = pgp(config);
 
 module.exports = db;
